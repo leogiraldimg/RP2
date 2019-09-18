5ecc3ab249a54562de1fb4759b644e6724fb52c8
leogiraldimg <leogiraldimg@usp.br>
2019-09-18 00:32:52 -0300

    Implementando novo objeto de busca

--- src/WordPress/searcher.py
+++ src/WordPress/searcher.py
@@ -1,24 +1,39 @@
 import json
+import unicodedata
 
 with open('data.json', 'r+') as f:
     data = json.load(f)
 
-with open('keywords.txt', 'r+') as f:
+with open('keywords_eler.txt', 'r+') as f:
     keywords = f.read()
 
 keywords_array = keywords.split(";")
 keywords_dict = { keywords_array[i] : 0 for i in range(0, len(keywords_array) ) }
-
-count = 0
+commit_obj = ""
 
 for commit in data:
     commit_message = commit["commit"]["message"]
+    keywords_string = ""
+    first_time = True
 
     for keyword in keywords_array:
         if commit_message.lower().find(keyword) != -1:
             keywords_dict[keyword] += 1
-            count = count + 1
 
-print("count:", count)
-print("\n")
-print("keywords_dict:", keywords_dict)
+            if first_time == True:
+                keywords_string += '"' + keyword + '"'
+                first_time = False
+            else:
+                keywords_string += ',"' + keyword + '"'
+
+    if keywords_string != "":           
+        try:
+            commit_obj_string = '{"id": "' + commit["sha"] + '", "message": "' + commit_message.replace("\n", "").replace('"', "").replace(':', "") + '", "url": "' + commit["html_url"] + '", "keywords": [' + keywords_string + ']}'
+            print(commit_obj_string)
+            commit_obj_loads = json.loads(commit_obj_string)
+            commit_obj = json.dumps(commit_obj_loads, indent=2, sort_keys=True)
+        except:
+            continue
+
+        with open('keywords_per_commit.json', 'a+') as f:
+            f.write(commit_obj)            
